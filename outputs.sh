21b4d61c9982a56099ef729c6a941745e6e93634
Simion Amalia Camelia <amalia.simion@stud.fils.upb.ro>
2023-05-06 10:41:36 +0300

    change tests

--- outputs.sh
+++ outputs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+classpath="./../libs/antlr-4.12.0-complete.jar:./../libs/jackson-annotations-2.15.0-rc3.jar:./../libs/jackson-core-2.15.0-rc3.jar:./../libs/jackson-databind-2.15.0-rc3.jar:./../libs/jackson-module-parameter-names-2.15.0-rc3.jar"
 for folder in tests/*
 do
     rm -rf "$folder"/*.alf.json
     rm -rf "$folder"/*.out
-    for file in "$folder"/*.ast.json
+    for file in "$folder"/*.alf.ast.json
     do
         echo $file
-        node ../index.js "$file" "$folder"/$(basename $file .ast.json).json
+#        node ../index.js "$file" "$folder"/$(basename $file .ast.json).json
+        java -cp $classpath:./../out org.example.Main $file "$folder"/$(basename $file .alf.ast.json).alf.json
     done
 done
