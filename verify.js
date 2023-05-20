a4587a1cc0dbc1814e45aa23ab1e77b188414ba8
Simion Amalia Camelia <amalia.simion@stud.fils.upb.ro>
2023-05-20 16:07:12 +0300

    fix tests

--- verify.js
+++ verify.js
@@ -31,41 +31,41 @@ catch (e)
 	process.exit (-1);
 }
 
-if (!originalTree.errors)
-{
+//if (!originalTree.errors)
+//{
 	assert.deepStrictEqual (originalTree, verifyTree);
-}
-else
-{
-	if (Array.isArray (verifyTree.errors))
-	{
-		if (originalTree.errors.length === verifyTree.errors.length) {
-			for (let error of originalTree.errors) {
-				let gotit = false;
-				for (let err of verifyTree.errors) {
-					if (error.id === err.id && error.line === err.line && error.position === err.position) {
-						gotit = true;
-					}
-				}
-				if (gotit === false) {
-					console.log (`You did not report ${JSON.stringify (error)}`);
-					process.exit (1);
-				}
-			}
-		}
-		else
-		{
-			console.log (`There should be ${originalTree.errors.length} errors, you reported  ${verifyTree.errors.length}`);
-			process.exit (1);
-		}
-		
-	}
-	else
-	{
-		console.log (`Errors should be an array, you have ${typeof (verifyTree.errors)}`);
-		process.exit (1);
-	}
-}
+//}
+//else
+//{
+//	if (Array.isArray (verifyTree.errors))
+//	{
+//		if (originalTree.errors.length === verifyTree.errors.length) {
+//			for (let error of originalTree.errors) {
+//				let gotit = false;
+//				for (let err of verifyTree.errors) {
+//					if (error.id === err.id && error.line === err.line && error.position === err.position) {
+//						gotit = true;
+//					}
+//				}
+//				if (gotit === false) {
+//					console.log (`You did not report ${JSON.stringify (error)}`);
+//					process.exit (1);
+//				}
+//			}
+//		}
+//		else
+//		{
+//			console.log (`There should be ${originalTree.errors.length} errors, you reported  ${verifyTree.errors.length}`);
+//			process.exit (1);
+//		}
+//
+//	}
+//	else
+//	{
+//		console.log (`Errors should be an array, you have ${typeof (verifyTree.errors)}`);
+//		process.exit (1);
+//	}
+//}
 
 
 
